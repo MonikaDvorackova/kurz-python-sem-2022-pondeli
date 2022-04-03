5b500d794b18d5660ebd380241575dd87c091bd3
MonikaDvorackova <95243292+MonikaDvorackova@users.noreply.github.com>
2022-04-03 16:53:26 +0200

    Update cteni_ze_souboru.py

--- cteni_ze_souboru.py
+++ cteni_ze_souboru.py
@@ -1,6 +1,6 @@
 
 # 1 Výplata přesněji 
-# Zatím jsme výplatu počítali za předpokladu, že každý měsíc odpracujeme stejný počet hodin, což není příliš realistické. Vytvořte proto textový soubor vykaz.txt, který bude obsahovat 12 řádků a na každém řádku počet odpracovaných hodin za každý měsíc za poslední rok.
+# Zatím jsme  výplatu počítali za předpokladu, že každý měsíc odpracujeme stejný počet hodin, což není příliš realistické. Vytvořte proto textový soubor vykaz.txt, který bude obsahovat 12 řádků a na každém řádku počet odpracovaných hodin za každý měsíc za poslední rok.
 # Otevřete tento soubor ve svém programu a načtěte hodnoty na řádcích do seznamu vykaz. Vytiskněte tento seznam na konzoli funkcí print() abyste si ověřili, že jste soubor načetli správně.
 # Nechte uživatele zadat na příkazovém řádku hodinovou mzdu. Spočítejte a na výstup vytiskněte celkovou výplatu za celý rok a průměrnou výplatu na jeden měsíc.
 
