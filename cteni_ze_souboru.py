5eff7781283ae6521acd1cff6a4c323363b80d4a
MonikaDvorackova <95243292+MonikaDvorackova@users.noreply.github.com>
2022-04-03 16:52:51 +0200

    Update cteni_ze_souboru.py

--- cteni_ze_souboru.py
+++ cteni_ze_souboru.py
@@ -155,7 +155,7 @@ def hody_kostkou(args):
 
 """
 Dny v měsíci
-Napište program, který bude mít přímo v kódu zapsaný počet dní v jednotlivých měsících takto:
+Napište  program, který bude mít přímo v kódu zapsaný počet dní v jednotlivých měsících takto:
 pocty_dni = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 Nechte váš program vypsat tento seznam do souboru s názvem kalendar.txt, každé číslo na jeden řádek.
 Upravte váš program tak, aby zároveň s počtem dnů vypisoval i název měsíce. Oddělte v souboru název měsíce a počet dnů pomocí tabulátoru.
